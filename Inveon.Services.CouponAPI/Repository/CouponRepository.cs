88801d48adee99f8193581b376badcc760add593
Uygar DOĞAN <uygar.dogan@bilgeadam.com>
2023-11-11 06:14:43 +0300

    Add project files.

---
+++ Inveon.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Inveon.Services.CouponAPI.DbContexts;
+using Inveon.Services.CouponAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inveon.Services.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _db;
+        protected IMapper _mapper;
+        public CouponRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponDto> GetCouponByCode(string couponCode)
+        {
+            var couponFromDb = await _db.Coupons.FirstOrDefaultAsync(u => u.CouponCode == couponCode);
+            if (couponFromDb == null)
+            {
+                CouponDto model = new CouponDto()
+                {
+                    CouponId = 0,
+                    CouponCode = "0",
+                    DiscountAmount = 0
+                };
+                return model;
+            }
+            return _mapper.Map<CouponDto>(couponFromDb);
+        }
+    }
+}
